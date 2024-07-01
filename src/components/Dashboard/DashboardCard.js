649aaa43118fdc491ccf719c71c5288100f1bfc8
Deepak k mayur <deepaktemporary20@gmail.com>
2024-07-01 12:27:01 +0530

    made changes in DashboardCard component commit

--- src/components/Dashboard/DashboardCard.js
+++ src/components/Dashboard/DashboardCard.js
@@ -1,3 +1,96 @@
+// import { Box, Typography } from '@mui/material';
+// import React from 'react';
+
+// const DashboardCard = ({ cardDetails }) => {
+
+//   return (
+//     <Box
+//       sx={{
+//         display: 'flex',
+//         gap: 2,
+//         justifyContent: 'space-evenly',
+//         // backgroundColor: "red",
+//         marginBottom: 2,
+//         padding: "5px 20px 0 20px",
+//         height: "70%",
+//         width: "100%",
+
+//       }}
+//     >
+//       {cardDetails.map((card, index) => (
+//         <Box
+//           key={index}
+//           sx={{
+//             display: "flex",
+//             flexDirection: "row",
+//             width: {
+//               xs: '80%',
+//               sm: '45%',
+//               md: '30%',
+//               lg: '20%',
+//               xl: '18%',
+//             },
+//             height: '100px',
+//             boxShadow: "0 0 20px rgba(0, 0, 0, 0.2)",
+//             borderRadius: "20px",
+//             margin: "10px",
+//             // backgroundColor: 'yellow',
+//             zIndex: 1
+//           }}
+//         >
+//           <Box
+         
+//             sx={{
+//               width: "100px",
+//               height: "100px",
+//               display: "flex",
+//               flexDirection: "column",
+//               alignItems: 'center',
+//               justifyContent: 'center',
+//               textAlign: 'center',
+//               padding: 1,
+//               position: 'relative',
+//               bottom: 2
+//             }}
+//           >
+//             <img src={card.image} alt="Image" style={{ width: '80%', height: 'auto', borderRadius: '10px', marginTop: '-15px' }} />
+//             <Typography sx={{ fontSize: "17px", color: `${card.color}`, fontWeight: "bold" }}>{card.percentage}%</Typography>
+//             <Typography sx={{ fontSize: "60%", fontWeight: "bold" }}>{card.view}</Typography>
+//           </Box>
+
+//           <Box
+
+//             sx={{
+//               width: "65%",
+//               display: "flex",
+//               flexDirection: "column",
+//               alignItems: "center",
+//               justifyContent: "center",
+//               paddingLeft: 1,
+//               textAlign: "center",
+//             }}
+//           >
+            
+//             <Typography sx={{ fontSize: "100%", color: `${card.color}`, fontWeight: "bold", marginBottom: "3px", marginLeft: "auto", marginRight: "10px" }}>
+//               {card.movement}
+//             </Typography>
+//             <Typography sx={{ fontSize: "120%", fontWeight: "bold", marginBottom: "3px", marginLeft: "auto", marginRight: "10px" }}>
+//               {card.number}
+//             </Typography>
+//             <Typography sx={{ fontSize: "60%", fontWeight: "bold", marginLeft: "auto", marginRight: "10px" }}>
+//               {card.viewMap}
+//             </Typography>
+//           </Box>
+//         </Box>
+//       ))}
+//     </Box>
+//   );
+// };
+
+// export default DashboardCard;
+
+
+
 import { Box, Typography } from '@mui/material';
 import React from 'react';
 
@@ -11,6 +104,7 @@ const DashboardCard = ({ cardDetails }) => {
         justifyContent: 'space-evenly',
         // backgroundColor: "red",
         marginBottom: 2,
+        marginTop: 2,
         padding: "5px 20px 0 20px",
         height: "70%",
         width: "100%",
@@ -39,9 +133,10 @@ const DashboardCard = ({ cardDetails }) => {
           }}
         >
           <Box
-         
+
             sx={{
-              width: "100px",
+              // width: "100px",
+              width: "80%",
               height: "100px",
               display: "flex",
               flexDirection: "column",
@@ -70,7 +165,7 @@ const DashboardCard = ({ cardDetails }) => {
               textAlign: "center",
             }}
           >
-            
+
             <Typography sx={{ fontSize: "100%", color: `${card.color}`, fontWeight: "bold", marginBottom: "3px", marginLeft: "auto", marginRight: "10px" }}>
               {card.movement}
             </Typography>
